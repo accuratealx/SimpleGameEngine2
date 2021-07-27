bc413ca4c3269f6e9e3c483829821508673a9e9a
AccurateAlx <accuratealx@gmail.com>
2021-07-27 23:49:41 +0300

    Добавлены файлы пакета для удобства подключения

---
+++ Package/SimpleGameEngine2.pas
@@ -0,0 +1,45 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit SimpleGameEngine2;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  GDIPAPI, GDIPOBJ, GDIPUTIL, OpenAL, dglOpenGL, sgeErrorManager, 
+  sgeNamedObjectList, sgeResourceList, sgeSystemFont, sgeWindow, 
+  SimpleGameEngine, sgeCriticalSection, sgeDateUtils, sgeErrors, sgeFile, 
+  sgeFileUtils, sgeKeys, sgeMemoryStream, sgeMetaInfoList, sgeOSPlatform, 
+  sgeSimpleCommand, sgeSimpleContainer, sgeSimpleParameters, 
+  sgeStartParameters, sgeStringList, sgeStringUtils, sgeSystemEvent, 
+  sgeSystemUtils, sgeThread, sgeTypes, sgeControllerList, sgeEventControllers, 
+  sgeEventGraphic, sgeEventList, sgeEventManager, sgeEventSubscriber, 
+  sgeEventSubscriberGroup, sgeEventSubscriberGroupList, 
+  sgeEventSubscriberList, sgeEventWindow, sgeExtensionControllers, 
+  sgeExtensionFileSystem, sgeExtensionGraphic, sgeExtensionList, 
+  sgeExtensionPackList, sgeExtensionResourceList, sgeExtensionShell, 
+  sgeExtensionSound, sgeExtensionStartParameters, sgeExtensionVariables, 
+  sgeExtensionWindow, sgeGraphicAnimation, sgeGraphicAnimationFrames, 
+  sgeGraphicBase, sgeGraphicColor, sgeGraphicDrawList, 
+  sgeGraphicElementAnimation, sgeGraphicElementBase, sgeGraphicElementCircle, 
+  sgeGraphicElementLayer, sgeGraphicElementLayerList, sgeGraphicElementList, 
+  sgeGraphicElementRectangle, sgeGraphicElementRectangleGradient, 
+  sgeGraphicElementSprite, sgeGraphicElementSpritePart, 
+  sgeGraphicElementSpriteTile, sgeGraphicElementText, 
+  sgeGraphicElementTextShadow, sgeGraphicFont, sgeGraphicFPS, 
+  sgeGraphicSprite, sgeGraphicSpriteGDIPLoader, sgeGraphicSpriteLoader, 
+  sgeGraphicUtils, sgeScreenFade, sgePackFileList, sgePackFileReader, 
+  sgePackFileWriter, sgeSoundBuffer, sgeSoundBufferLoader, 
+  sgeSoundBufferWavLoader, sgeSoundSource, sgeTemplateCollection, 
+  sgeTemplateList, sgeSystemCursor, sgeSystemIcon, sgeSystemTimeEvent, 
+  sgeSystemTrayIcon, sgeVariableBoolean, sgeVariableBooleanBase, 
+  sgeVariableBooleanVirtual, sgeVariableInteger, sgeVariableIntegerBase, 
+  sgeVariableIntegerVirtual, sgeVariableList, sgeVariableSingle, 
+  sgeVariableSingleBase, sgeVariableSingleVirtual, sgeVariableString, 
+  sgeVariableStringBase, sgeVariableStringVirtual;
+
+implementation
+
+end.
