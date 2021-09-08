256f098c0ba3f680b0a0ce57a8a5967e93ccc247
AccurateAlx <accuratealx@gmail.com>
2021-09-09 01:37:27 +0300

    Добавлены глобальные переменные для GUI

---
+++ Source/System/sgeVars.pas
@@ -0,0 +1,28 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeVars.pas
+Версия            1.0
+Создан            09.09.2021
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          Глобальные переменные
+}
+
+{$Include Defines.inc}
+unit sgeVars;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  SimpleGameEngine;
+
+
+var
+  SGE: TSimpleGameEngine;                                           //Ссылка на основной класс
+
+
+implementation
+
+end.
+
