96ec0aae5b420fe25d3d11287ce979b21e1881d8
AccurateAlx <accuratealx@gmail.com>
2021-09-10 00:29:06 +0300

    В форму добавлен графический элемент для отрисовки

--- Source/System/GUI/sgeGUIForm.pas
+++ Source/System/GUI/sgeGUIForm.pas
@@ -7,6 +7,7 @@
 Описание          GUI: Форма
 }
 {$Include Defines.inc}
+{$ModeSwitch duplicatelocals+}
 
 unit sgeGUIForm;
 
@@ -15,17 +16,70 @@ unit sgeGUIForm;
 interface
 
 uses
-  sgeGUIElement;
+  sgeGUIElement, sgeGraphicElementSpriteCashed;
 
 
 type
   TsgeGUIForm = class(TsgeGUIElement)
   private
+    FGraphicElement: TsgeGraphicElementSpriteCashed;
+
+  protected
+    procedure DrawBefore; override;
+
   public
+    constructor Create(AName: String; ALeft, ATop, AWidth, AHeight: Integer; AParent: TsgeGUIElement = nil); override;
+    destructor  Destroy; override;
+
+    procedure Draw; override;
   end;
 
 
 implementation
+
+uses
+  sgeVars, sgeGraphicColor;
+
+
+
+procedure TsgeGUIForm.DrawBefore;
+begin
+  with SGE.ExtGraphic.Graphic do
+    begin
+    BGColor := cRed;
+    EraseBG;
+    end;
+end;
+
+
+constructor TsgeGUIForm.Create(AName: String; ALeft, ATop, AWidth, AHeight: Integer; AParent: TsgeGUIElement);
+begin
+  inherited Create(AName, ALeft, ATop, AWidth, AHeight, AParent);
+
+  //Создать графический элемент
+  FGraphicElement := TsgeGraphicElementSpriteCashed.Create(Left, Top, Width, Height, FCanvas);
+  SGE.ExtGraphic.DrawList.AddElement(FGraphicElement, 'GUI');
+
+  //Перерисовать форму
+  Repaint;
+end;
+
+
+destructor TsgeGUIForm.Destroy;
+begin
+  //Удалить графический элемент
+  FGraphicElement.Delete;
+
+  inherited Destroy;
+end;
+
+
+procedure TsgeGUIForm.Draw;
+begin
+  inherited Draw;
+
+  if FGraphicElement <> nil then FGraphicElement.Update;
+end;
 
 end.
 
