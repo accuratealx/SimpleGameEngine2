06197ee9fb59b58609320f84afeb619a5f33558c
AccurateAlx <accuratealx@gmail.com>
2021-09-23 00:55:51 +0300

    Добавлены методы IndexOf и Delete

--- Source/System/GUI/sgeGUIFormList.pas
+++ Source/System/GUI/sgeGUIFormList.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeGUIFormList.pas
-Версия            1.0
+Версия            1.1
 Создан            04.09.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          GUI: Список форм
@@ -25,10 +25,43 @@ type
   TsgeGUIFormList = class(TsgeGUIFormListTemplate)
   private
   public
+    function  IndexOf(Form: TsgeGUIForm): Integer;
+
+    procedure Delete(Index: Integer);
+    procedure Delete(Form: TsgeGUIForm);
   end;
 
 
 implementation
+
+
+
+function TsgeGUIFormList.IndexOf(Form: TsgeGUIForm): Integer;
+var
+  i: Integer;
+begin
+  Result := -1;
+
+  for i := 0 to FCount - 1 do
+    if FList[i] = Form then Exit(i);
+end;
+
+
+procedure TsgeGUIFormList.Delete(Index: Integer);
+begin
+  inherited Delete(Index);
+end;
+
+
+procedure TsgeGUIFormList.Delete(Form: TsgeGUIForm);
+var
+  Idx: Integer;
+begin
+  Idx := IndexOf(Form);
+  if Idx <> -1 then inherited Delete(Idx);
+end;
+
+
 
 end.
 
