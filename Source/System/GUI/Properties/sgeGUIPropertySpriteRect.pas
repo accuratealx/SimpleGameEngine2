8e358729f2de9c14446ac8d20fdbb41702eb5d58
AccurateAlx <accuratealx@gmail.com>
2021-10-09 22:53:32 +0300

    Добавлена функция загрузки параметров

--- Source/System/GUI/Properties/sgeGUIPropertySpriteRect.pas
+++ Source/System/GUI/Properties/sgeGUIPropertySpriteRect.pas
@@ -1,3 +1,13 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeGUIPropertySpriteRect.pas
+Версия            1.1
+Создан            01.10.2021
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          GUI: Свойство: Часть спрайта для вывода
+}
+{$Include Defines.inc}
+
 unit sgeGUIPropertySpriteRect;
 
 {$mode objfpc}{$H+}
@@ -5,7 +15,7 @@ unit sgeGUIPropertySpriteRect;
 interface
 
 uses
-  sgeTypes, sgeGraphicSprite,
+  sgeTypes, sgeSimpleParameters, sgeGraphicSprite,
   sgeGUIProperty, sgeGUIPropertyIntPoint, sgeGUIPropertyFloatRect;
 
 
@@ -17,22 +27,26 @@ type
   TsgeGUIPropertySpriteRect = class(TsgeGUIProperty)
   private
     FMode: TsgeGUIPropertySpriteRectMode;
-    FTile: TsgeGUIPropertyIntPoint;                                 //Координаты плитки
-    FRect: TsgeGUIPropertyFloatRect;                                //Координаты прямоугольника
+    FTile: TsgeGUIPropertyIntPointExt;                              //Координаты плитки
+    FRect: TsgeGUIPropertyFloatRectExt;                             //Координаты прямоугольника
 
     procedure SetMode(AMode: TsgeGUIPropertySpriteRectMode);
+
+    function GetTile: TsgeGUIPropertyIntPoint;
+    function GetRect: TsgeGUIPropertyFloatRect;
   public
     constructor Create(AOwner: TObject); override;
     destructor  Destroy; override;
 
     property Mode: TsgeGUIPropertySpriteRectMode read FMode write SetMode;
-    property Tile: TsgeGUIPropertyIntPoint read FTile;
-    property Rect: TsgeGUIPropertyFloatRect read FRect;
+    property Tile: TsgeGUIPropertyIntPoint read GetTile;
+    property Rect: TsgeGUIPropertyFloatRect read GetRect;
   end;
 
 
   TsgeGUIPropertySpriteRectExt = class(TsgeGUIPropertySpriteRect)
   public
+    procedure LoadParameters(Parameters: TsgeSimpleParameters; Prefix: String = '');
     function GetRect(Sprite: TsgeGraphicSprite): TsgeFloatRect;     //Прямоугольник в координатах OpenGL
   end;
 
@@ -52,12 +66,24 @@ begin
 end;
 
 
+function TsgeGUIPropertySpriteRect.GetTile: TsgeGUIPropertyIntPoint;
+begin
+  Result := FTile;
+end;
+
+
+function TsgeGUIPropertySpriteRect.GetRect: TsgeGUIPropertyFloatRect;
+begin
+  Result := FRect
+end;
+
+
 constructor TsgeGUIPropertySpriteRect.Create(AOwner: TObject);
 begin
   inherited Create(AOwner);
 
-  FTile := TsgeGUIPropertyIntPoint.Create(AOwner);
-  FRect := TsgeGUIPropertyFloatRect.Create(AOwner);
+  FTile := TsgeGUIPropertyIntPointExt.Create(AOwner);
+  FRect := TsgeGUIPropertyFloatRectExt.Create(AOwner);
 
   FMode := srmFull;
 end;
@@ -69,6 +95,30 @@ begin
   FTile.Free;
 
   inherited Destroy;
+end;
+
+
+procedure TsgeGUIPropertySpriteRectExt.LoadParameters(Parameters: TsgeSimpleParameters; Prefix: String);
+var
+  ParamName, s: String;
+begin
+  //Mode
+  ParamName := Prefix + 'Mode';
+  if Parameters.Exist[ParamName] then
+    begin
+    s := LowerCase(Parameters.GetValue(ParamName, ''));
+    case s of
+      'full': FMode := srmFull;
+      'tile': FMode := srmTile;
+      'rect': FMode := srmRect;
+    end;
+    end;
+
+  //Tile
+  FTile.LoadParameters(Parameters, Prefix + 'Tile.');
+
+  //Rect
+  FRect.LoadParameters(Parameters, Prefix + 'Rect.');
 end;
 
 
