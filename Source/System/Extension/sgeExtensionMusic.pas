cf08781be2253e47e30984b08524b514875afc41
AccurateAlx <accuratealx@gmail.com>
2021-10-18 23:38:21 +0300

    Добавлены свойства поведения проигрывателя. Добавлены объекты для проигрывания музыки.

--- Source/System/Extension/sgeExtensionMusic.pas
+++ Source/System/Extension/sgeExtensionMusic.pas
@@ -15,6 +15,7 @@ unit sgeExtensionMusic;
 interface
 
 uses
+  sgeThread, sgeSoundSource, sgeSoundBuffer,
   sgeExtensionBase,
   sgeMusicPLayerTrackList;
 
@@ -24,10 +25,40 @@ const
 
 
 type
+  //Режим проигрывания дорожек (Случайно, Вперёд, Назад)
+  TsgeExtensionMusicPlayMode = (empmRandom, empmForward, empmBackward);
+
+
+  //Режим повторения дорожек (Дорожка, Список)
+  TsgeExtensionMusicRepeatMode = (emrpTrack, emrpList);
+
+
+  //Состояние проигрывателя (Проигрывается, Остановлен)
+  TsgeExtensionMusicSate = (emsPlay, emsStop);
+
+
   TsgeExtensionMusic = class(TsgeExtensionBase)
   private
+    //Классы
+    FThread: TsgeThread;
     FTrackList: TsgeMusicPlayerTrackList;
+    FSource: TsgeSoundSource;
+    FBuffer: TsgeSoundBuffer;
 
+    //Параметры
+    FPlayMode: TsgeExtensionMusicPlayMode;                          //Режим проигрывания
+    FRepeatmode: TsgeExtensionMusicRepeatMode;                      //Режим повторения
+    FGroup: String;                                                 //Текущая группа
+    FFadeTime: Word;                                                //Время затухания и нарастания в мс
+
+    //Вспомогательные параметры
+    FFadeSeconds: Single;                                           //Время затухания и нарастания в секундах
+
+    procedure ThreadProc;
+
+    //Свойства
+    procedure SetFadeTime(ATime: Word);
+    function  GetState: TsgeExtensionMusicSate;
   protected
     class function GetName: String; override;
 
@@ -35,6 +66,14 @@ type
     constructor Create(ObjectList: TObject); override;
     destructor  Destroy; override;
 
+    procedure Play;
+    procedure Stop;
+
+    property PlayMode: TsgeExtensionMusicPlayMode read FPlayMode write FPlayMode;
+    property Repeatmode: TsgeExtensionMusicRepeatMode read FRepeatmode;
+    property Group: String read FGroup write FGroup;
+    property FadeTime: Word read FFadeTime write SetFadeTime;
+    property State: TsgeExtensionMusicSate read GetState;
     property TrackList: TsgeMusicPlayerTrackList read FTrackList;
   end;
 
@@ -48,6 +87,30 @@ const
   _UNITNAME = 'ExtensionMusic';
 
 
+procedure TsgeExtensionMusic.ThreadProc;
+begin
+
+end;
+
+
+procedure TsgeExtensionMusic.SetFadeTime(ATime: Word);
+begin
+  if FFadeTime = ATime then Exit;
+
+  FFadeTime := ATime;
+
+  //Посчитать время затухания в секундах
+  FFadeSeconds := FFadeTime / 1000;
+end;
+
+
+function TsgeExtensionMusic.GetState: TsgeExtensionMusicSate;
+begin
+  if FSource.State = sssPlay then Result := emsPlay
+    else Result := emsStop;
+end;
+
+
 class function TsgeExtensionMusic.GetName: String;
 begin
   Result := Extension_Music;
@@ -59,8 +122,26 @@ begin
   try
     inherited Create(ObjectList);
 
-    //Список дорожек
+    //Создать классы
     FTrackList := TsgeMusicPlayerTrackList.Create(True);
+    FThread := TsgeThread.Create;
+
+    FSource := TsgeSoundSource.Create;
+    FSource.RelativePos := True;
+    FSource.Gain := 1;
+
+    FBuffer := TsgeSoundBuffer.CreateBlank;
+
+    //Установить параметры
+    FPlayMode := empmRandom;
+    FRepeatmode := emrpList;
+    FGroup := '';
+
+    //Задать время затухания
+    SetFadeTime(2000);
+
+    //Установить метод потока
+    FThread.LoopProc := @ThreadProc;
 
   except
     on E: EsgeException do
@@ -71,9 +152,35 @@ end;
 
 destructor TsgeExtensionMusic.Destroy;
 begin
+  //Остановить проигрывание
+  FSource.Stop;
+  FSource.Buffer := nil;
+
+  //Удалить классы
+  FThread.Free;
+  FBuffer.Free;
+  FSource.Free;
   FTrackList.Free;
 
   inherited Destroy;
+end;
+
+
+procedure TsgeExtensionMusic.Play;
+begin
+  FTrackList.GetRandomTrack;
+  FBuffer.LoadFromFile(FTrackList.CurrentTrack.FileName);
+  FSource.Buffer := FBuffer;
+  FSource.Play;
+end;
+
+
+procedure TsgeExtensionMusic.Stop;
+begin
+  if GetState = emsStop then Exit;
+
+  FSource.Stop;
+  FSource.Buffer := nil;
 end;
 
 
