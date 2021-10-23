6f615ca106157305cfea0ba3905876299170fe12
AccurateAlx <accuratealx@gmail.com>
2021-10-23 15:36:17 +0300

    Мелкие исправления

--- Source/System/Graphic/sgeGraphicElementLayer.pas
+++ Source/System/Graphic/sgeGraphicElementLayer.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeGraphicElementLayer.pas
-Версия            1.0
+Версия            1.1
 Создан            14.07.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          Класс слоя графических элементов
@@ -22,9 +22,11 @@ type
   TsgeGraphicElementLayer = class
   private
     FElementList: TsgeGraphicElementList;     //Список объектов
+
     FName: ShortString;                       //Имя
     FVisible: Boolean;                        //Видимость
     FIndex: Word;                             //Индекс приоритета
+
   public
     constructor Create(Name: ShortString; Index: Word = 0; Visible: Boolean = True);
     destructor  Destroy; override;
@@ -32,6 +34,7 @@ type
     property Name: ShortString read FName;
     property Index: Word read FIndex;
     property Visible: Boolean read FVisible write FVisible;
+
     property Elements: TsgeGraphicElementList read FElementList;
   end;
 
@@ -41,7 +44,7 @@ implementation
 
 constructor TsgeGraphicElementLayer.Create(Name: ShortString; Index: Word; Visible: Boolean);
 begin
-  FElementList := TsgeGraphicElementList.Create;
+  FElementList := TsgeGraphicElementList.Create(True);
 
   FName := Name;
   FIndex := Index;
