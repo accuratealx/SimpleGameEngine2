64c8b2e7d078e4ed2212f69983b0486be4f8c8ab
AccurateAlx <accuratealx@gmail.com>
2021-10-28 22:03:32 +0300

    Доработан базовый класс задачи

--- Source/System/MusicPlayer/sgeMusicPLayerTaskBase.pas
+++ Source/System/MusicPlayer/sgeMusicPLayerTaskBase.pas
@@ -4,7 +4,7 @@
 Версия            1.0
 Создан            21.10.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
-Описание          Music player: Базовая задача
+Описание          Music player: Задача: Базовая
 }
 {$Include Defines.inc}
 
@@ -16,13 +16,29 @@ interface
 
 type
   TsgeMusicPlayerTaskBase = class
-  protected
-    FMusicPLayer: TObject;
-    FDone: Boolean;
-  public
-    constructor Create(ExtMusicPLayerObj: TObject);
+  type
+    TTaskState = (tsBegin, tsWork, tsEnd);
 
-    procedure Work; virtual; abstract;
+  private
+    function GetTimes: Integer;
+
+  protected
+    FState: TTaskState;                                             //Внутреннее состояние
+    FDone: Boolean;                                                 //Флаг завершения
+    FTimes: Integer;                                                //Количество срабатываний задачи
+    FDeltaTime: Single;                                             //Шаг изменения громкости
+
+    function CalculateDeltaTime(Current, New: Single; Times: Cardinal): Single;
+
+    //Методы задачи
+    function  GetDeltaTime: Single; virtual;
+    procedure Start; virtual;
+    procedure Work; virtual;
+    procedure Stop; virtual;
+  public
+    constructor Create;
+
+    procedure Process;
 
     property Done: Boolean read FDone;
   end;
@@ -30,12 +46,107 @@ type
 
 implementation
 
+uses
+  sgeVars,
+  sgeExtensionMusicPlayer;
 
-constructor TsgeMusicPlayerTaskBase.Create(ExtMusicPLayerObj: TObject);
+type
+  TsgeExtMusicPlayerExt = class(TsgeExtensionMusicPlayer);
+
+
+function TsgeMusicPlayerTaskBase.GetTimes: Integer;
+var
+  d: Single;
+  ExtMusic: TsgeExtMusicPlayerExt;
 begin
-  FMusicPLayer := ExtMusicPLayerObj;
+  ExtMusic := TsgeExtMusicPlayerExt(SGE.ExtMusicPlayer);
+
+  d := ExtMusic.FFadeTime / ExtMusic.FThreadDelay;
+  if d < 1 then d := 1;
+
+  Result := Round(d);
+end;
+
+
+function TsgeMusicPlayerTaskBase.CalculateDeltaTime(Current, New: Single; Times: Cardinal): Single;
+begin
+  Result := (New - Current) / Times;
+end;
+
+
+function TsgeMusicPlayerTaskBase.GetDeltaTime: Single;
+begin
+  Result := 1;
+end;
+
+
+procedure TsgeMusicPlayerTaskBase.Start;
+begin
+  //Начало
+end;
+
+
+procedure TsgeMusicPlayerTaskBase.Work;
+var
+  ExtMusic: TsgeExtMusicPlayerExt;
+begin
+  //Ссылка на расширение
+  ExtMusic := TsgeExtMusicPlayerExt(SGE.ExtMusicPlayer);
+
+  //Изменить громкость
+  ExtMusic.FSource.Gain := ExtMusic.FSource.Gain + FDeltaTime;
+end;
+
+
+procedure TsgeMusicPlayerTaskBase.Stop;
+begin
+  //Конец
+end;
+
+
+constructor TsgeMusicPlayerTaskBase.Create;
+begin
+  FState := tsBegin;
   FDone := False;
 end;
+
+
+procedure TsgeMusicPlayerTaskBase.Process;
+begin
+  if FDone then Exit;
+
+  //Начало задачи
+  if FState = tsBegin then
+    begin
+    //Установить количество срабатываний
+    FTimes := GetTimes;
+
+    //Определить шаг изменения громкости
+    FDeltaTime := GetDeltaTime;
+
+    //Начало задачи
+    Start;
+
+    //Следующий шаг
+    FState := tsWork;
+    end;
+
+  //Процесс
+  if FState = tsWork then
+    begin
+    Work;
+    Dec(FTimes);
+    if FTimes = 0 then FState := tsEnd;
+    end;
+
+  //Конец задачи
+  if FState = tsEnd then
+    begin
+    Stop;
+    FDone := True;
+    end;
+end;
+
 
 
 end.
