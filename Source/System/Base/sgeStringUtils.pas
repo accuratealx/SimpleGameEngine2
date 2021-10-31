81950823df9901591ccf29c5d00d6a7db37e7236
AccurateAlx <accuratealx@gmail.com>
2021-10-31 20:26:41 +0300

    Добавлен метод поиска индекса строки по имени

--- Source/System/Base/sgeStringUtils.pas
+++ Source/System/Base/sgeStringUtils.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeStringUtils.pas
-Версия            1.0
+Версия            1.1
 Создан            14.05.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          Вспомогательные функции для работы со строками
@@ -15,7 +15,7 @@ unit sgeStringUtils;
 interface
 
 uses
-  sgeSimpleParameters;
+  sgeStringList, sgeSimpleParameters;
 
 
 type
@@ -26,11 +26,13 @@ type
 
 function  sgeStringReplace(const S, OldPattern, NewPattern: string; Flags: TsgeReplaceFlags): string;
 function  sgeSubstituteParamsToString(Str: String; Parameters: TsgeSimpleParameters; OpenQuote: String = ''; CloseQuote: String = ''): String;
+function  sgeGetListIndexByValue(List: TsgeStringList; Value: String): Integer;
+
 
 implementation
 
 uses
-  SysUtils;
+  SysUtils, sgeSystemUtils;
 
 
 function sgeStringReplace(const S, OldPattern, NewPattern: string; Flags: TsgeReplaceFlags): string;
@@ -48,6 +50,18 @@ begin
   c := Parameters.Count - 1;
   for i := 0 to c do
     Result := StringReplace(Result, OpenQuote + Parameters.Parameter[i].Name + CloseQuote, Parameters.Parameter[i].Value, [SysUtils.rfIgnoreCase, SysUtils.rfReplaceAll]);
+end;
+
+
+function sgeGetListIndexByValue(List: TsgeStringList; Value: String): Integer;
+var
+  i: Integer;
+begin
+  Result := -1;
+
+  Value := sgeTrim(Value);
+  for i := 0 to List.Count - 1 do
+    if LowerCase(List.Part[i]) = Value then Exit(i);
 end;
 
 
