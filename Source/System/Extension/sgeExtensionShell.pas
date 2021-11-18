b0aa103dc7ba0de9d7c551e9b43521ee283a4871
AccurateAlx <accuratealx@gmail.com>
2021-11-18 22:29:50 +0300

    Исправлен цвет вывода поясняющих строк при выводе ошибки

--- Source/System/Extension/sgeExtensionShell.pas
+++ Source/System/Extension/sgeExtensionShell.pas
@@ -943,7 +943,7 @@ begin
 
   //Добавить в журнал строки, первая ошибка
   for i := 0 to List.Count - 1 do
-    if i = 0 then LogMessage(List.Part[i], smtError) else LogMessage('  ' + List.Part[i], smtError);
+    if i = 0 then LogMessage(List.Part[i], smtError) else LogMessage('  ' + List.Part[i], smtNote);
 
   //Почистить память
   List.Free;
