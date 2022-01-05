832acb2bf85f9730043fa50f9f6f2d5130f85867
AccurateAlx <accuratealx@gmail.com>
2022-01-06 01:10:09 +0300

    Добавлен модуль с константами GUI

---
+++ Source/System/GUI/sgeGUITypes.pas
@@ -0,0 +1,26 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeGUITypes.pas
+Версия            1.0
+Создан            30.11.2021
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          GUI: Типы
+}
+{$Include Defines.inc}
+
+unit sgeGUITypes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+type
+  //Ориентация компонента
+  TsgeGUIOrientation = (oHorizontal, oVertical);
+
+
+
+implementation
+
+end.
+
