ed0d5468386e44ba1630ade636ec997462d89cd9
AccurateAlx <accuratealx@gmail.com>
2022-05-15 22:11:13 +0300

    Добавлен идентификатор перехода

--- Source/System/Events/sgeEventGraphic.pas
+++ Source/System/Events/sgeEventGraphic.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeEventGraphic.pas
-Версия            1.1
+Версия            1.2
 Создан            06.05.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          Классы событий: Графика
@@ -27,24 +27,27 @@ type
   TsgeEventGraphicFade = class(TsgeEventBase)
   private
     FPassedTime: TsgePassedTime;
+    FID: Integer;
   public
-    constructor Create(Name: ShortString; PassedTime: TsgePassedTime);
+    constructor Create(Name: ShortString; PassedTime: TsgePassedTime; ID: Integer);
 
     property PassedTime: TsgePassedTime read FPassedTime;
+    property ID: Integer read FID;
   end;
-
 
 
 
 implementation
 
 
-constructor TsgeEventGraphicFade.Create(Name: ShortString; PassedTime: TsgePassedTime);
+constructor TsgeEventGraphicFade.Create(Name: ShortString; PassedTime: TsgePassedTime; ID: Integer);
 begin
   inherited Create(Name);
 
   FPassedTime := PassedTime;
+  FID := ID;
 end;
+
 
 
 end.
