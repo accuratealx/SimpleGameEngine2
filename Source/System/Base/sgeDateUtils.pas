f422e4463dae3b4b96a1282a06d238b7d5967036
AccurateAlx <accuratealx@gmail.com>
2022-12-21 01:30:32 +0300

    Добавлена функция получения текущей даты и времени

--- Source/System/Base/sgeDateUtils.pas
+++ Source/System/Base/sgeDateUtils.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeDateUtils.pas
-Версия            1.0
+Версия            1.1
 Создан            14.05.2021
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          Функции работы с датой
@@ -20,6 +20,8 @@ type
 
 function sgeNow: TDateTime;
 function sgeFormatDateTime(const FormatStr: string; DateTime: TDateTime): string;
+function sgeGetTimeStamp: String;
+
 
 
 implementation
@@ -38,6 +40,13 @@ function sgeFormatDateTime(const FormatStr: string; DateTime: TDateTime): string
 begin
   Result := SysUtils.FormatDateTime(FormatStr, DateTime);
 end;
+
+
+function sgeGetTimeStamp: String;
+begin
+  Result := sgeFormatDateTime('yyyy.mm.dd-hh.nn.ss', Now);
+end;
+
 
 
 end.
