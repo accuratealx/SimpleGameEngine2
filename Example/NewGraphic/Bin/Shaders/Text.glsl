7368eb1585a7847172c7fc7773fa9f8ce00481ef
accuratealx <accuratealx@gmail.com>
2023-03-26 23:27:57 +0300

    Исправлены синтаксические ошибки

--- Example/NewGraphic/Bin/Shaders/Text.glsl
+++ Example/NewGraphic/Bin/Shaders/Text.glsl
@@ -3,7 +3,7 @@
 layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
-out vec2 TexCoord;              //Координаты тектуры
+out vec2 TexCoord;              //Координаты текстуры
 
 uniform vec2 ScreenSize;        //Размеры экрана
 uniform vec4 Layer;             //Положение слоя - xy, масштаб - zw
