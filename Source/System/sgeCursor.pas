03f65e52f21589ef3d36804ebbfa973b632e3f00
accuratealx <accuratealx@gmail.com>
2023-07-05 23:26:40 +0300

    Переписан класс TsgeCursor

--- Source/System/sgeCursor.pas
+++ Source/System/sgeCursor.pas
@@ -1,7 +1,7 @@
 {
 Пакет             Simple Game Engine 2
 Файл              sgeCursor.pas
-Версия            1.0
+Версия            1.1
 Создан            06.01.2022
 Автор             Творческий человек  (accuratealx@gmail.com)
 Описание          Графический курсор
@@ -16,24 +16,26 @@ interface
 
 uses
   sgeTypes,
-  sgeGraphicAnimationFrames;
+  sgeSprite, sgeAnimationFrameList;
 
 
 type
   TsgeCursor = class
   private
-    FFrames: TsgeGraphicAnimationFrames;
+    FSprite: TsgeSprite;
+    FFrames: TsgeAnimationFrameList;
     FHotPoint: TsgeIntPoint;
     FWidth: Integer;
     FHeight: Integer;
 
   public
-    constructor Create(Frames: TsgeGraphicAnimationFrames; Width, Height: Integer; HotPointX: Integer = 0; HotPointY: Integer = 0);
+    constructor Create(Sprite: TsgeSprite; Frames: TsgeAnimationFrameList; Width, Height: Integer; HotPointX: Integer; HotPointY: Integer);
 
-    property HotPoint: TsgeIntPoint read FHotPoint;
-    property Frames: TsgeGraphicAnimationFrames read FFrames;
-    property Width: Integer read FWidth;
-    property Height: Integer read FHeight;
+    property Sprite: TsgeSprite read FSprite write FSprite;
+    property Frames: TsgeAnimationFrameList read FFrames write FFrames;
+    property HotPoint: TsgeIntPoint read FHotPoint write FHotPoint;
+    property Width: Integer read FWidth write FWidth;
+    property Height: Integer read FHeight write FHeight;
   end;
 
 
@@ -46,13 +48,25 @@ const
   _UNITNAME = 'Cursor';
 
   Err_EmptyFrames = 'EmptyFrames';
+  Err_EmptySprite = 'EmptySprite';
+  Err_ZeroFrames  = 'ZeroFrames';
 
 
-constructor TsgeCursor.Create(Frames: TsgeGraphicAnimationFrames; Width, Height: Integer; HotPointX: Integer; HotPointY: Integer);
+constructor TsgeCursor.Create(Sprite: TsgeSprite; Frames: TsgeAnimationFrameList; Width, Height: Integer; HotPointX: Integer; HotPointY: Integer);
 begin
+  //Проверить спрайт
+  if Sprite = nil then
+    raise EsgeException.Create(_UNITNAME, Err_EmptySprite);
+
+  //Проверить кадры анимации
   if Frames = nil then
     raise EsgeException.Create(_UNITNAME, Err_EmptyFrames);
 
+  //Проверить количество кадров
+  if Frames.Count < 1 then
+    raise EsgeException.Create(_UNITNAME, Err_ZeroFrames);
+
+  FSprite := Sprite;
   FFrames := Frames;
   FWidth := Width;
   FHeight := Height;
