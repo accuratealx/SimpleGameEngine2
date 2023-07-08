de6ba524647178007b5c7f4cd59c5da83b0085ec
accuratealx <accuratealx@gmail.com>
2023-07-08 03:14:03 +0300

    Добавлен класс слоя OpenGL

---
+++ Source/System/Graphic/OpenGL/sgeGraphicOpenGLLayer.pas
@@ -0,0 +1,85 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeGraphicElementLayer.pas
+Версия            1.2
+Создан            14.07.2021
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          OpenGL: Слой
+}
+{$Include Defines.inc}
+
+unit sgeGraphicOpenGLLayer;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  sgeTypes,
+  sgeDisplayLayer;
+
+
+type
+  TsgeGraphicElementLayer = class
+  private
+    FData: TsgeDisplayLayerData;
+
+  public
+    constructor Create(Layer: TsgeDisplayLayer);
+    destructor  Destroy; override;
+
+    procedure Update(Layer: TsgeDisplayLayer);
+
+    property Name: String read FData.Name;
+    property Index: Word read FData.Index;
+    property Visible: Boolean read FData.Visible;
+    property Offset: TsgeFloatPoint read FData.Offset;
+    property Scale: TsgeFloatPoint read FData.Scale;
+  end;
+
+
+implementation
+
+
+constructor TsgeGraphicElementLayer.Create(Layer: TsgeDisplayLayer);
+begin
+  Update(Layer);
+end;
+
+
+destructor TsgeGraphicElementLayer.Destroy;
+begin
+
+end;
+
+
+procedure TsgeGraphicElementLayer.Update(Layer: TsgeDisplayLayer);
+begin
+  //Видимость
+  if dlcsName in Layer.ChangeSet then
+    FData.Name := Layer.Data.Name;
+
+  //Видимость
+  if dlcsVisible in Layer.ChangeSet then
+    FData.Visible := Layer.Data.Visible;
+
+  //Приоритет
+  if dlcsIndex in Layer.ChangeSet then
+  begin
+    FData.Index := Layer.Data.Index;
+    //Отсортировать слои как нибудь
+  end;
+
+  //Смещение
+  if dlcsOffset in Layer.ChangeSet then
+    FData.Offset := Layer.Data.Offset;
+
+  //Масштаб
+  if dlcsScale in Layer.ChangeSet then
+    FData.Scale := Layer.Data.Scale;
+end;
+
+
+
+end.
+
