72e8fc12cac3339133c4fee23bf09c086279e341
accuratealx <accuratealx@gmail.com>
2023-08-02 01:27:25 +0300

    Добавлен прямоугольник

---
+++ Source/System/GUI/sgeGUIRect.pas
@@ -0,0 +1,84 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeGUIRect.pas
+Версия            1.0
+Создан            27.07.2023
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          GUI: Прямоугольник
+}
+{$Include Defines.inc}
+
+unit sgeGUIRect;
+
+{$mode ObjFPC}{$H+}
+{$ModeSwitch duplicatelocals+}
+
+interface
+
+uses
+  sgeGraphicColor,
+  sgeGUIElement,
+  sgeDisplayElementRect;
+
+type
+  TsgeGUIRect = class(TsgeGUIElement)
+  private
+    FDisplayElement: TsgeDisplayElementRect;
+
+  protected
+    procedure DisplayElement_CorrectPosition(RealLeft, RealTop: Integer); override;
+    procedure DisplayElement_CorrectSize(Width, Height: Integer); override;
+    procedure DisplayElement_CorrectVisible(Visible: Boolean); override;
+
+  public
+    constructor Create(Name: String; Left, Top, Width, Height: Integer; Color: TsgeColor; Visible: Boolean = True; Parent: TsgeGUIElement = nil);
+    destructor  Destroy; override;
+  end;
+
+
+implementation
+
+
+procedure TsgeGUIRect.DisplayElement_CorrectPosition(RealLeft, RealTop: Integer);
+begin
+  FDisplayElement.PositionX := RealLeft;
+  FDisplayElement.PositionY := RealTop;
+  FDisplayElement.Update;
+end;
+
+
+procedure TsgeGUIRect.DisplayElement_CorrectSize(Width, Height: Integer);
+begin
+  FDisplayElement.Width := Width;
+  FDisplayElement.Height := Height;
+  FDisplayElement.Update;
+end;
+
+
+procedure TsgeGUIRect.DisplayElement_CorrectVisible(Visible: Boolean);
+begin
+  FDisplayElement.Visible := Visible;
+end;
+
+
+constructor TsgeGUIRect.Create(Name: String; Left, Top, Width, Height: Integer; Color: TsgeColor; Visible: Boolean; Parent: TsgeGUIElement);
+begin
+  FDisplayElement := TsgeDisplayElementRect.Create(Left, Top, Left + Width, Top + Height, Color);
+  FDisplayElement.Add(Layer_GUI_Name);
+
+  inherited Create(Name, Left, Top, Width, Height, Visible, Parent);
+end;
+
+
+destructor TsgeGUIRect.Destroy;
+begin
+  FDisplayElement.Delete;
+  FDisplayElement.Free;
+
+  inherited Destroy;
+end;
+
+
+
+end.
+
