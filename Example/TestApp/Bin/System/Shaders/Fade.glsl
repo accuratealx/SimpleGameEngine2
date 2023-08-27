c421f2d04a71013b52baf8f9754a5f55cecd0b92
accuratealx <accuratealx@gmail.com>
2023-08-27 14:19:46 +0300

    Удален лишний мусор

--- Example/TestApp/Bin/System/Shaders/Fade.glsl
+++ Example/TestApp/Bin/System/Shaders/Fade.glsl
@@ -2,19 +2,9 @@
 
 layout (location = 0) in vec2 aPos;
 
-const vec4 points[] = vec4[]
-(
-    vec4(-1.0, 1.0, 0.0, 1.0),
-    vec4(-1.0, -1.0, 0.0, 1.0),
-    vec4(1.0, -1.0, 0.0, 1.0),
-    vec4(-1.0, 1.0, 0.0, 1.0),
-    vec4(1.0, -1.0, 0.0, 1.0),
-    vec4(1.0,  1.0, 0.0, 1.0)
-);
-
 void main()
 {
-    gl_Position = points[gl_VertexID];
+    gl_Position = vec4(aPos.xy, 0.0, 0.0);
 }
 
 
