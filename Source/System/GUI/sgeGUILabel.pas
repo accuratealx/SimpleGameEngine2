0a7aec6544975c8fe9bf0fc34bcec8470d0f2d01
accuratealx <accuratealx@gmail.com>
2023-11-27 23:59:18 +0300

    Добавлен GUI элемент sgeGUILabel (Не все свойства реализованы)

---
+++ Source/System/GUI/sgeGUILabel.pas
@@ -0,0 +1,192 @@
+{
+Пакет             Simple Game Engine 2
+Файл              sgeGUILabel.pas
+Версия            1.0
+Создан            27.11.2023
+Автор             Творческий человек  (accuratealx@gmail.com)
+Описание          GUI: Надпись
+}
+{$Include Defines.inc}
+
+unit sgeGUILabel;
+
+{$mode ObjFPC}{$H+}
+{$ModeSwitch duplicatelocals+}
+
+interface
+
+uses
+  sgeTypes, sgeAnsiFont, sgeColor,
+  sgeDisplayElementAnsiText, sgeDisplayElementRect,
+  sgeGUIElement;
+
+type
+  TsgeGUILabel = class(TsgeGUIElement)
+  protected
+    FBGRect: TsgeDisplayElementRect;
+    FText: TsgeDisplayElementAnsiText;
+    FVerticalAlign: TsgeVerticalAlign;
+    FHorizontalAlign: TsgeHorizontalAlign;
+
+
+    procedure GetTextSize(out Width, Height: Integer);
+    procedure CorrectTextPosition;
+
+    procedure SetVerticalAlign(AAlign: TsgeVerticalAlign);
+    procedure SetHorizontalAlign(AAlign: TsgeHorizontalAlign);
+
+    procedure CalculateAutosize(var NewWidth, NewHeight: Integer); override;
+    procedure CheckMinimalSize(var NewWidth, NewHeight: Integer); override;
+
+    procedure DisplayElement_CorrectPosition(RealLeft, RealTop: Integer); override;
+    procedure DisplayElement_CorrectSize(Width, Height: Integer); override;
+    procedure DisplayElement_CorrectVisible(Visible: Boolean); override;
+    function  DisplayElement_GetVisible: Boolean; override;
+    procedure DisplayElement_CorrectClipRect(Rect: TsgeClipRect); override;
+    function  DisplayElement_GetClipRect: TsgeClipRect; override;
+  public
+    constructor Create(Name: String; Left, Top, Width, Height: Integer; AText: String; Font: TsgeAnsiFont; Color: TsgeColor; Visible: Boolean; Parent: TsgeGUIElement);
+    destructor  Destroy; override;
+
+    property VerticalAlign: TsgeVerticalAlign read FVerticalAlign write SetVerticalAlign;
+    property HorizontalAlign: TsgeHorizontalAlign read FHorizontalAlign write SetHorizontalAlign;
+  end;
+
+
+implementation
+
+
+procedure TsgeGUILabel.GetTextSize(out Width, Height: Integer);
+begin
+  Width := Round(FText.Font.GetTextWidth(FText.Text) * FText.Scale);
+  Height := Round(FText.Font.GetTextHeight(FText.Text) * FText.Scale);
+end;
+
+
+procedure TsgeGUILabel.CorrectTextPosition;
+var
+  Size, GlobalPos: TsgeIntPoint;
+  W, H: Integer;
+begin
+  //Размеры текста с учетом масштаба
+  GetTextSize(W, H);
+
+  //Размер элемента с учетом масштаба
+  Size := GetScaleSize;
+
+  //Реальное положение на экране
+  GlobalPos := GetGlobalPos;
+
+  //Изменить выводимый элемент
+  FText.PositionX := GlobalPos.X + sgeGetHorizontalAlignOffset(FHorizontalAlign, Size.X, W);
+  FText.PositionY := GlobalPos.Y + sgeGetVerticaAlignOffset(FVerticalAlign, Size.Y, H);
+  FText.Scale := GetScale;
+  FText.Update;
+end;
+
+
+procedure TsgeGUILabel.SetVerticalAlign(AAlign: TsgeVerticalAlign);
+begin
+  if FVerticalAlign = AAlign then
+    Exit;
+
+  FVerticalAlign := AAlign;
+
+  CorrectTextPosition;
+end;
+
+
+procedure TsgeGUILabel.SetHorizontalAlign(AAlign: TsgeHorizontalAlign);
+begin
+  if FHorizontalAlign = AAlign then
+    Exit;
+
+  FHorizontalAlign := AAlign;
+
+  CorrectTextPosition;
+end;
+
+
+procedure TsgeGUILabel.CalculateAutosize(var NewWidth, NewHeight: Integer);
+begin
+  GetTextSize(NewWidth, NewHeight);
+end;
+
+
+procedure TsgeGUILabel.CheckMinimalSize(var NewWidth, NewHeight: Integer);
+var
+  W, H: Integer;
+begin
+  GetTextSize(W, H);
+
+  if NewWidth < W then
+    NewWidth := W;
+
+  if NewHeight < H then
+    NewHeight := H;
+end;
+
+
+procedure TsgeGUILabel.DisplayElement_CorrectPosition(RealLeft, RealTop: Integer);
+begin
+
+  CorrectTextPosition;
+end;
+
+
+procedure TsgeGUILabel.DisplayElement_CorrectSize(Width, Height: Integer);
+begin
+  CorrectTextPosition;
+end;
+
+
+procedure TsgeGUILabel.DisplayElement_CorrectVisible(Visible: Boolean);
+begin
+  FText.Visible := Visible;
+end;
+
+
+function TsgeGUILabel.DisplayElement_GetVisible: Boolean;
+begin
+  Result := FText.Visible;
+end;
+
+
+procedure TsgeGUILabel.DisplayElement_CorrectClipRect(Rect: TsgeClipRect);
+begin
+  FText.ClipRect := Rect;
+end;
+
+
+function TsgeGUILabel.DisplayElement_GetClipRect: TsgeClipRect;
+begin
+  Result := FText.ClipRect;
+end;
+
+
+constructor TsgeGUILabel.Create(Name: String; Left, Top, Width, Height: Integer; AText: String; Font: TsgeAnsiFont; Color: TsgeColor; Visible: Boolean; Parent: TsgeGUIElement);
+begin
+  FVerticalAlign := vaCenter;
+  FHorizontalAlign := haCenter;
+
+  //Отображаемый элемент
+  FText := TsgeDisplayElementAnsiText.Create(Left, Top, Color, Font, AText);
+  FText.Add(Layer_GUI_Name);
+  FText.ClipRect := sgeGetClipRect(Left, Top, Width, Height);
+  FText.Clipped := True;
+
+  inherited Create(Name, Left, Top, Width, Height, Visible, Parent);
+end;
+
+
+destructor TsgeGUILabel.Destroy;
+begin
+  FText.Delete;
+  FText.Free;
+
+  inherited Destroy;
+end;
+
+
+end.
+
